e134438864967e9ecf49df67c03a07c07dc1b848
armandoLopz <armandolopez0611@gmail.com>
2024-06-18 08:21:56 -0400

    Add find Cart by ID

--- springboot-practices/src/main/java/com/armando/pruebas/springboot_practices/controllers/ShoppingController.java
+++ springboot-practices/src/main/java/com/armando/pruebas/springboot_practices/controllers/ShoppingController.java
@@ -1,36 +1,39 @@
 package com.armando.pruebas.springboot_practices.controllers;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.armando.pruebas.springboot_practices.models.Product;
 import com.armando.pruebas.springboot_practices.models.ShoppingCart;
-import com.armando.pruebas.springboot_practices.services.ProductServices;
 import com.armando.pruebas.springboot_practices.services.ShoppingCartServices;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-//@RequestMapping("/user/{userId}/shopping")
-@RequestMapping("/all")
+
+@RequestMapping("/user/{id}")
 public class ShoppingController {
 
     @Autowired
-    private ProductServices productServices;
-    
-    @Autowired
     private ShoppingCartServices shoppingCartServices;
-    @GetMapping("")
-    public List<Product> productsList() {
-        
-        return productServices.AllProducts();
+    
+    @GetMapping("/cart")
+    public ResponseEntity<Object> findCartByIdUser(@PathVariable int id) {
+
+        return shoppingCartServices.findShoppingCartById(id);
     }
 
+    @GetMapping("/addProduct/{idProduct}")
+    public Product addProduct(@PathVariable int idProduct) {
+
+        return null;
+    }
+    
     @GetMapping("/sh")
     public Map<String, ShoppingCart> allShoppingCarts() {
         return shoppingCartServices.showAllShoppingCarts();
--- springboot-practices/src/main/java/com/armando/pruebas/springboot_practices/services/ShoppingCartServices.java
+++ springboot-practices/src/main/java/com/armando/pruebas/springboot_practices/services/ShoppingCartServices.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.armando.pruebas.springboot_practices.models.ShoppingCart;
@@ -29,4 +31,26 @@ public class ShoppingCartServices {
         return mapJson;
     }
     
+
+    public ResponseEntity<Object> findShoppingCartById(int id){
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        
+        shoppingCart = shoppingCartRepository.showAllShoppingCarts().entrySet()
+            .stream()
+            .filter(entry -> entry.getKey().getId() ==id)
+            .map(Map.Entry::getValue)
+            .findFirst()
+            .orElse(null);
+
+        if (shoppingCart != null) {
+            return ResponseEntity.ok(shoppingCart);
+        
+        } else {
+            
+            String errorMessage = "No se encontró el carrito de compras para el usuario con ID: " + id;
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+        }   
+    }
 }
